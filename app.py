242d2444f1bf9519273b8b5aa7207f88b09a278c
Toan <tdctkz@umsystem.edu>
2022-09-23 14:19:12 -0500

    form update

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # create a Flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "donation"
+
+
+# Create a form class
+class NameForm(FlaskForm):
+    name = StringField("Your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create a route decorator
+# Create Login pages
 @app.route('/')
 
-def index():
-    return render_template("index.html")
+def login():
+    return render_template("login.html")
+
+# Create Sign up pages
+@app.route('/sign_up')
+
+def sign_up():
+    return render_template("sign_up.html")
+
+# Create Name pages
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
 
 # Create Custom Error pages
-
 # Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
---
+++ static/script.js
@@ -0,0 +1,57 @@
+/ Delete notes function/
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
+/ Show password function /
+function show_Password() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function password_show_hide() {
+  var x = document.getElementById("password1");
+  var show_eye = document.getElementById("show_eye");
+  var hide_eye = document.getElementById("hide_eye");
+  hide_eye.classList.remove("d-none");
+  if (x.type === "password") {
+    x.type = "text";
+    show_eye.style.display = "none";
+    hide_eye.style.display = "block";
+  } else {
+    x.type = "password";
+    show_eye.style.display = "block";
+    hide_eye.style.display = "none";
+  }
+}
+
+/ Remember username funciton/
+const rmCheck = document.getElementById("rememberMe"),
+  emailInput = document.getElementById("email");
+
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+  rmCheck.setAttribute("checked", "checked");
+  emailInput.value = localStorage.username;
+} else {
+  rmCheck.removeAttribute("checked");
+  emailInput.value = "";
+}
+
+function lsRememberMe() {
+  if (rmCheck.checked && emailInput.value !== "") {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = rmCheck.value;
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
