3826d3e0618b3e1f6e6ec947da22af56be4dd13c
Toan <tdctkz@umsystem.edu>
2022-10-29 11:04:05 -0500

    update

--- static/script.js
+++ static/script.js
@@ -45,27 +45,20 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-  (function (fn) {
-    'use strict';
-    fn(window.jQuery, window, document);
-  }(function ($, window, document) {
-    'use strict';
-
-    $(function () {
-      $('.sort-btn').on('click', '[data-sort]', function (event) {
-        event.preventDefault();
-
-        var $this = $(this),
-          sortDir = 'desc';
-
-        if ($this.data('sort') !== 'asc') {
-          sortDir = 'asc';
-        }
-
-        $this.data('sort', sortDir).find('.fa').attr('class', 'fa fa-sort-' + sortDir);
-
-        // call sortDesc() or sortAsc() or whathaveyou...
-      });
-    });
-  }));
+function myFunction() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
 
